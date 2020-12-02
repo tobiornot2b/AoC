a2d088e15aeb09678b2ac49580a4ea69263d9e8a
tobiornot2b <tobias.maede@gmail.com>
2020-12-02 18:48:32 +0100

    Refactor day 01

--- 2020/days/day01.hs
+++ 2020/days/day01.hs
@@ -3,16 +3,26 @@ import System.IO
 main = do
     stringNumbers <- readFile "inputs/day01.txt" 
     let numbers = makeInteger $ lines $ stringNumbers
-    let allCombinationsWithSum = [ (x,y,z, x+y+z) | x <- numbers, y <- numbers, z <- numbers]
-    let sumIs2020 = filter (\(x,y,z,sum) -> sum == 2020) allCombinationsWithSum
-    let res = sumIs2020 !! 0
-    let prod = (first res) * (second res) * (third res)
-    print prod
+    print (part1 numbers)
+    print (part2 numbers)
     return ()
+
+part1 :: [Int] -> Int
+part1 xs = 
+    let  newList = [ (x,y, x+y) | x <- xs, y <- xs ]
+     in prodFirst2 $ (!! 0) $ filter (\(x,y,sum) -> sum == 2020) $ newList
+
+part2 :: [Int] -> Int
+part2 xs = 
+    let newList = [ (x,y,z, x+y+z) | x <- xs, y <- xs, z <- xs ]
+     in prodFirst3 $ (!! 0) $ filter (\(x,y,z,sum) -> sum == 2020) $ newList
+
+prodFirst2 :: (Int,Int,a) -> Int
+prodFirst2 (x,y,_) = x * y
+
+prodFirst3 :: (Int,Int,Int,a) -> Int
+prodFirst3 (x,y,z,_) = x * y * z
 
 makeInteger :: [String] -> [Int]
 makeInteger stringNumbers = map read stringNumbers
 
-first (x,_,_,_) = x
-second (_,x,_,_) = x
-third (_,_,x,_) = x
